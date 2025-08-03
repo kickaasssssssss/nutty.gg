a5749012c238beb111cee950d96323f8f2d4d779
kickaasssssssss <107225784+kickaasssssssss@users.noreply.github.com>
2025-08-03 15:48:17 +0700

    Update script.js

--- multichat-overlay/script.js
+++ multichat-overlay/script.js
@@ -406,7 +406,7 @@ function TikfinityConnect() {
 	
 	if (tikfinityWebsocket) return; // Already connected
 
-	tikfinityWebsocket = new WebSocket("ws://localhost:21213/");
+	tikfinityWebsocket = new WebSocket("ws://localhost:62024/");
 
 	tikfinityWebsocket.onopen = function () {
 		console.log(`TikFinity successfully connected...`)
